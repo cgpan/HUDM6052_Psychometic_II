84ce89e96d7652870edb9d51ca7bbd416f54c153
cgpan <grepan3280@gmail.com>
2023-11-16 14:07:59 -0500

    ## Q1-a-(3)
    ## Q1-a-(3) finished

--- assignment 4/HW4_test.R
+++ assignment 4/HW4_test.R
@@ -41,6 +41,9 @@ irt_1pl <- mirt(df, 1, SE=T)
 irt_1pl
 coef(irt_1pl, IRTpars=T, simplify=T)
 
+irt_2pl <- mirt(df, model = 1, itemtype = "2PL", SE=T)
+irt_2pl
+
 spec <- 'F = 1-33
 PRIOR = (1-33, g, norm, -1.1, 2)'
 irt_3pl <- mirt(df, model = spec, itemtype = "3PL", SE=T)
@@ -48,7 +51,18 @@ irt_3pl
 parameters <- coef(irt_3pl, IRTpars=T, simplify = T)
 as.data.frame(parameters$items)  
 
-D = -2*(-20260.22+20078.62)
+D = -2*(-20078.62+20064.48)
 
-pchisq(D, df = 32, lower.tail = FALSE)
+pchisq(D, df = 33, lower.tail = FALSE)
 
+item_fit_1pl <- itemfit(irt_1pl, na.rm = T)
+item_fit_2pl <- itemfit(irt_2pl, na.rm = T)
+item_fit_3pl <- itemfit(irt_3pl, na.rm = T)
+names(item_fit_1pl)
+item_fit_all <- cbind(item_fit_1pl[,c("item")],
+                      round(item_fit_1pl[,c("S_X2", "p.S_X2")],4),
+                      round(item_fit_2pl[,c("S_X2", "p.S_X2")],4),
+                      round(item_fit_3pl[,c("S_X2", "p.S_X2")],4))
+names(item_fit_all)[1] <- "item"
+write.csv(item_fit_all, file = "/Users/panpeter/Desktop/PhD_Learning/HUDM6052 Psychometric II/HUDM6052_Psychometic_II/assignment 4/item_fit_all.csv",
+          row.names = F)
